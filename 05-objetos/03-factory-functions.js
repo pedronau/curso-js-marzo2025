be7ff6a2e59f79cae5876ce2ea79cbd16441afad
pedronau <pedropefer34@gmail.com>
2025-03-22 21:47:53 +0100

    terminado video 51

--- 05-objetos/03-factory-functions.js
+++ 05-objetos/03-factory-functions.js
@@ -1,3 +1,5 @@
+//las factory functions nos permiten crear objetos de una forma sencilla y no repetitiva
+
 let user = { 
     id: 1,
     email: 'pedro@holamundo.es',
@@ -7,3 +9,29 @@ let user = {
         console.log('Recuperando clave...');
     }, 
 }
+
+let user1 = { 
+    id: 2,
+    email: 'juan@holamundo.es',
+    name: 'Juan',
+    activo: false,
+    recuperarClave: function () { 
+        console.log('Recuperando clave...');
+    }, 
+}
+
+//para crear un objeto usuario nuevo.... en vez de hacer copia pega...
+
+function crearUsuario(name, email) {
+    return { 
+        email, //si tienes un nombre igual que una propiedad lo puedes dejar asi en vez de email: email, (como esta hecho el name)
+        name: name,
+        activo: true,
+        recuperarClave: function () { 
+            console.log('Recuperando clave...');
+        },
+    }
+}
+
+let user2 = crearUsuario('Pedro', 'pedro@gemail.com');
+let user3 = crearUsuario('Felipe', 'felipe@gmail.com');
