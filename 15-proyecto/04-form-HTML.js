1fa32e0ea42b0c1b01194c2bba0a9c845714ea03
pedronau <pedropefer34@gmail.com>
2025-03-31 20:14:57 +0200

    CURSO TERMINADO. 167 VÍDEOS.

--- 15-proyecto/04-form-HTML.js
+++ 15-proyecto/04-form-HTML.js
@@ -3,6 +3,10 @@ class User {
     static #users = [];
     static #ul = document.createElement('ul');
     static #form = document.createElement('form');
+    static #initialValues = {
+        name: '',
+        email: '',
+    }
 
     static async getAll() {
         try {
@@ -32,9 +36,30 @@ class User {
         event.preventDefault();
     }
 
+    static formHTML({ data, errors }) {
+        return `
+        <form>
+            <div>
+                <label>Nombre:</label>
+                <input name="name" value="${data.name}" />
+                ${errors.name || ''}
+            </div>
+            <div>
+                <label>Correo:</label>
+                <input name="email" value="${data.email}" />
+                ${errors.email || ''}
+            </div>
+            <input type="submit" value="Enviar" />
+        </form>
+        `
+    }
+
     static renderForm() {
         this.#form.onsubmit = this.onSubmit;
-        this.#form.innerHTML = '';
+        this.#form.innerHTML = this.formHTML({
+            data: this.#initialValues,
+            errors: {},
+        });
 
         return this.#form;
     }
